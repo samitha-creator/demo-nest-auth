cb6029120545b75402ea5b53533749e31e6420a7
Kokila Ekanayake <kokilaprabuddha@gmail.com>
2025-12-28 13:24:19 +0530

    Add register endpoint to AuthController
    Introduces a new POST /auth/register endpoint in AuthController. Also refactors imports in app.module.ts for improved readability.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() input: { username: string; password: string }) {
     return this.authService.authenticate(input);
+  }
+
+  @Post('register')
+  register(@Body() input: {}) {
+    
   }
 }
